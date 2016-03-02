b60a355435f8c066e5dcc3aefdd319b5740e074e
emmabeynon <emma.beynon@gmail.com>
2016-03-02 17:50:43 +0000

    Added a validation to require the presence of a category in order to create a record in the accounts table

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ActiveRecord::Base
   message = "Only alphanumeric characters and underscores accepted"
   validates :username, presence: true, uniqueness: true, length: {maximum: 15},
-  format: { with: /\A[A-Za-z0-9\_]/, message: message}
+  format: { with: /\A[A-Za-z0-9\_]/, message: message }
+  validates :category, presence: true
 end
--- spec/models/account_model_spec.rb
+++ spec/models/account_model_spec.rb
@@ -1,28 +1,39 @@
 require 'rails_helper'
 
 describe Account, type: :model do
-  it 'does not accept duplicate account usernames' do
-    account1 = Account.create(username: 'thearchieparker')
-    account2 = Account.new(username: 'thearchieparker')
-    expect(account2).to have(1).error_on(:username)
-    expect(account2).not_to be_valid
+  describe 'username' do
+    it 'does not accept duplicate account usernames' do
+      account1 = Account.create(username: 'thearchieparker', category: 'Eating out')
+      account2 = Account.new(username: 'thearchieparker', category: 'Eating out')
+      expect(account2).to have(1).error_on(:username)
+      expect(account2).not_to be_valid
+    end
+
+    it 'does not accept a blank username' do
+      account = Account.new(username: nil )
+      expect(account).to have(2).errors_on(:username)
+      expect(account).not_to be_valid
+    end
+
+    it 'only accepts usernames with alphanumeric characters and underscores' do
+      account = Account.new(username: '@username')
+      expect(account).to have(1).error_on(:username)
+      expect(account).not_to be_valid
+    end
+
+    it 'does not accept usernames longer than 15 characters' do
+      account = Account.new(username: 'alongusername123')
+      expect(account).to have(1).error_on(:username)
+      expect(account).not_to be_valid
+    end
   end
 
-  it 'does not accept a blank username' do
-    account = Account.new(username: nil )
-    expect(account).to have(2).errors_on(:username)
-    expect(account).not_to be_valid
+  describe 'category' do
+    it 'requires a category to create an account entry' do
+      account = Account.new(username: 'username')
+      expect(account).to have(1).error_on(:category)
+      expect(account).not_to be_valid
+    end
   end
 
-  it 'only accepts usernames with alphanumeric characters and underscores' do
-    account = Account.new(username: '@username')
-    expect(account).to have(1).error_on(:username)
-    expect(account).not_to be_valid
-  end
-
-  it 'does not accept usernames longer than 15 characters' do
-    account = Account.new(username: 'alongusername123')
-    expect(account).to have(1).error_on(:username)
-    expect(account).not_to be_valid
-  end
 end
