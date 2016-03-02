33d089309e8f2ad307ae3a59766d8f474d350224
emmabeynon <emma.beynon@gmail.com>
2016-03-02 17:14:26 +0000

    Added a validation for usernames that should only accept alphanumeric characters and underscores

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
 class Account < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
+  validates_format_of :username, with: /\A[A-Za-z0-9\_]/
 end
--- spec/models/account_model_spec.rb
+++ spec/models/account_model_spec.rb
@@ -9,7 +9,13 @@ describe Account, type: :model do
   end
 
   it 'does not accept a blank username' do
-    account = Account.new(username: '')
+    account = Account.new(username: nil )
+    expect(account).to have(2).errors_on(:username)
+    expect(account).not_to be_valid
+  end
+
+  it 'only accepts usernames with alphanumeric characters and underscores' do
+    account = Account.new(username: '@username')
     expect(account).to have(1).error_on(:username)
     expect(account).not_to be_valid
   end
