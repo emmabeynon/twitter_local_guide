bd134c15b5e4f8a2c20d5193df68bb662f52df40
emmabeynon <emma.beynon@gmail.com>
2016-03-02 17:56:15 +0000

    Added more 'add account' feature specs

--- spec/features/accounts_feature_spec.rb
+++ spec/features/accounts_feature_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
-
-feature 'Add account' do
+# As a user
+# So that I can see tweets from local accounts that I know already
+# I would like to add a Twitter account to the app
+feature 'Feature: Add account' do
   context 'a valid account' do
     scenario 'allow user to add an account and categeory' do
       add_account('signalse23', 'Eating out')
@@ -18,6 +20,16 @@ feature 'Add account' do
     scenario 'does not allow user to submit a blank account username' do
       add_account('', 'Drinking')
       expect(page).to have_content 'There was an error. Please try again.'
+    end
+
+    scenario 'does not allow user to submit a username with invalid characters' do
+      add_account('@username', 'Drinking')
+      expect(page).to have_content 'There was an error. Please try again'
+    end
+
+    scenario 'does not allow user to submit a username longer than 15 characters' do
+      add_account('alongusername123', 'Drinking')
+      expect(page).to have_content 'There was an error. Please try again'
     end
   end
 end
